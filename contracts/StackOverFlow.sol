3a2f1ba3c05b2e5279e8ae061e0bc3a75c1b2c09
Noor Mohammed B <noormohammed.b@gpckasaragod.ac.in>
2022-06-07 21:10:25 +0530

    Contract Improved

--- contracts/StackOverFlow.sol
+++ contracts/StackOverFlow.sol
@@ -1,51 +1,84 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >= 0.8.12;
+pragma solidity >=0.8.12;
 
-contract StackOverFlow{
-    // constructor(){
-    // }
+contract Stackoverflow {
+    address public adminAddress;
 
-    uint private globalQuestionID  = 0;
-
-
-    struct voteStruct{
-        int upvote;
-        int downvote;
+    constructor() {
+        adminAddress = msg.sender;
     }
 
-    struct questionStruct{
-        uint id;
+    uint256 public globalQuestionID = 0;
+    uint256 public globalAnsID = 0;
+
+    struct VoteStruct {
+        int256 upvote;
+        int256 downvote;
+    }
+    struct AnswerStruct {
+        uint256 id;
+        uint256 questionId;
+        uint256 date;
+        string answer;
+        // string[] tags;
+        address replayer;
+        VoteStruct vote;
+    }
+
+    struct QuestionStruct {
+        uint256 id;
         address asker;
-        uint date;
+        uint256 date;
         string heading;
         string question;
         string[] tags;
-        voteStruct vote;
-    }
-    
-    struct answerStruct{
-        uint id;
-        uint questionId;
-        uint date;
-        address replayer;
-        voteStruct vote;
+        AnswerStruct[] answersArray;
+        VoteStruct vote;
     }
 
-    questionStruct[] public questions;
-    
-    
-    function askQuestion(string memory qsHead, string memory qsCont, string[] memory qsTag) external returns(uint) {
+    // mapping(uint256 => QuestionStruct) public questions;
+    QuestionStruct[] public questions;
+
+    function askQuestion(
+        string memory qsHead,
+        string memory qsCont,
+        string[] memory qsTag
+    ) external returns (uint256) {
+        globalQuestionID++;
+
+        // AnswerStruct[] memory answers;
+        VoteStruct memory votesOfQuestion = VoteStruct(0, 0);
         questions.push(
-            questionStruct(
-                globalQuestionID++,
+            QuestionStruct(
+                // questions[globalQuestionID] = QuestionStruct(
+                globalQuestionID,
                 msg.sender,
                 block.timestamp,
                 qsHead,
                 qsCont,
                 qsTag,
-                voteStruct(0,0)
+                new AnswerStruct[](0),
+                votesOfQuestion
+                // );
             )
         );
         return globalQuestionID;
+    }
+
+    function answerQuestion(uint256 _qsID, string memory _ans)
+        external
+    // string[] memory tags
+    {
+        questions[_qsID].answersArray.push(
+            AnswerStruct(
+                globalAnsID++,
+                globalQuestionID,
+                block.timestamp,
+                _ans,
+                // tags,
+                msg.sender,
+                VoteStruct(0, 0)
+            )
+        );
     }
 }
